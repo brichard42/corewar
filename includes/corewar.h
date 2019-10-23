11396016a1e9536f7fab0cddbd48320de6f134a4
Bastien RICHARD-CANAVAGGIO <brichard@e2r12p14.42.fr>
2019-10-23 14:09:08 +0200

    added free_process to .h and organised .h with description of aded prototypes

--- includes/corewar.h
+++ includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: paullaurent <paullaurent@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 15:20:45 by plaurent          #+#    #+#             */
-/*   Updated: 2019/10/23 14:03:05 by paullaurent      ###   ########.fr       */
+/*   Updated: 2019/10/23 14:08:12 by brichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,11 @@ typedef struct		s_vm
 t_process			init_process(int32_t reg_1, int32_t pc);
 t_process			*create_process(int32_t reg_1, int32_t pc);
 void				delete_process(t_process *to_delete);
+void				free_process(t_process **to_delete);
+
+/*
+**	-------COREWAR STRUCT FUNCTIONS-------
+*/
 t_vm    			*create_vm(void);
 t_vm    			init_vm(void);
 void    			delete_vm(t_vm *to_delete);
