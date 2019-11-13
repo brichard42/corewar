c03357fc0f8e4c9a3313485be273e9c504ee3628
Thomas LANDEMAINE <tlandema@e2r12p10.42.fr>
2019-11-13 13:50:50 +0100

    Added some features to the process list!

--- vm/srcs/visualisator/draw/draw_process_list.c
+++ vm/srcs/visualisator/draw/draw_process_list.c
@@ -6,26 +6,35 @@
 /*   By: tlandema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 17:51:01 by tlandema          #+#    #+#             */
-/*   Updated: 2019/11/12 13:31:18 by tlandema         ###   ########.fr       */
+/*   Updated: 2019/11/13 13:48:50 by tlandema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-int8_t	draw_process_list(t_window *win, t_vm *env)
+int8_t	draw_process_list(t_window *win, t_vm *env, int champ_num)
 {
 	SDL_Rect	pos;
 	SDL_Point	point;
 	int			text[3];
+	int			tmp;
+	char		*str;
 
-	point = create_point(1955, 660);
-	if (create_tab_int3(text, 30, TEXT2, BOLD) == FAILURE)
+	point = create_point(1925, 665);
+	if (create_tab_int3(text, 20, TEXT2, BOLD) == FAILURE)
 		return (FAILURE);
 	pos = create_rect(1810, 650, 490, 50);
 	if (draw_rectangle(win, pos, create_color(50, 50, 44, 255)) == FAILURE)
 		return (FAILURE);
-	if (draw_text(win, "Process List :", point, text) == FAILURE)
+	if ((tmp = draw_text(win, "Process List of champ Num ", point, text))
+			&& tmp == FAILURE)
 		return (FAILURE);
+	point.x += tmp;
+	if ((str = ft_lltoa(env->champ[champ_num].champ_n)) == NULL)
+		return (FAILURE);
+	if ((draw_text(win, str, point, text)) == FAILURE)
+		return (FAILURE);
+	ft_strdel(&str);
 	pos = create_rect(1810, 705, 490, 585);
 	if (draw_rectangle(win, pos, create_color(50, 50, 44, 255)) == FAILURE)
 		return (FAILURE);
--- vm/srcs/visualisator/draw/main_drawer.c
+++ vm/srcs/visualisator/draw/main_drawer.c
@@ -6,13 +6,13 @@
 /*   By: tlandema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 15:12:46 by tlandema          #+#    #+#             */
-/*   Updated: 2019/11/12 18:02:17 by tlandema         ###   ########.fr       */
+/*   Updated: 2019/11/13 13:46:47 by tlandema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-int8_t	draw(t_window *win, t_vm *env, int space_counter)
+int8_t	draw(t_window *win, t_vm *env, int space_counter, int champ_counter)
 {
 	int	count;
 
@@ -26,7 +26,7 @@ int8_t	draw(t_window *win, t_vm *env, int space_counter)
 		return (FAILURE);
 	if (draw_arena(win, env, 0) == FAILURE)
 		return (FAILURE);
-	if (draw_process_list(win, env) == FAILURE)
+	if (draw_process_list(win, env, champ_counter) == FAILURE)
 		return (FAILURE);
 	while (count < 4)
 		if (draw_champions(win, env, count++) == FAILURE)
@@ -34,7 +34,8 @@ int8_t	draw(t_window *win, t_vm *env, int space_counter)
 	return (SUCCESS);
 }
 
-void	event_catcher(t_window *win, int *space_counter, uint8_t *play)
+void	event_catcher(t_window *win, int *space_counter, int *champ_counter,
+		uint8_t *play)
 {
 	if (win->event.type == SDL_QUIT)
 		*play = 0;
@@ -44,23 +45,31 @@ void	event_catcher(t_window *win, int *space_counter, uint8_t *play)
 	if ((win->event.type == SDL_KEYUP
 			&& win->event.key.keysym.sym == SDLK_SPACE))
 		*space_counter = (*space_counter == 0 || *space_counter == 2) ? 1 : 2;
+	if ((win->event.type == SDL_KEYUP
+			&& win->event.key.keysym.sym == SDLK_RIGHT))
+		*champ_counter = (*champ_counter < 3) ? *champ_counter + 1 : 0;
+	if ((win->event.type == SDL_KEYUP
+			&& win->event.key.keysym.sym == SDLK_LEFT))
+		*champ_counter = (*champ_counter > 0) ? *champ_counter - 1 : 3;
 }
 
 int8_t	drawer(t_window *win, t_vm *env)
 {
 	int		ret;
 	int		space_counter;
+	int		champ_counter;
 	uint8_t	play;
 
 	play = 1;
 	space_counter = 0;
+	champ_counter = 0;
 	while (play)
 	{
-		if (draw(win, env, space_counter) == FAILURE)
+		if (draw(win, env, space_counter, champ_counter) == FAILURE)
 			return (FAILURE);
 		ret = SDL_PollEvent(&(win->event));
 		if (ret != 0)
-			event_catcher(win, &space_counter, &play);
+			event_catcher(win, &space_counter, &champ_counter, &play);
 		SDL_RenderPresent(win->renderer);
 	}
 	return (SUCCESS);
