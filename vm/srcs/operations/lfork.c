37ebd3f7ac6ac53824c0ed9bbadf81c8706691a8
Thomas LANDEMAINE <tlandema@e2r10p4.42.fr>
2019-11-27 18:50:23 +0100

    Function : lfork.

--- vm/srcs/operations/lfork.c
+++ vm/srcs/operations/lfork.c
@@ -1,6 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lfork.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlandema <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/27 18:42:49 by tlandema          #+#    #+#             */
+/*   Updated: 2019/11/27 18:42:52 by tlandema         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "corewar"
 
 void    lfork(t_vm *vm, t_process *process)
 {
-	return ;
+    if (copy_process(process->pc + (process->op.param[0]), &process) == FAILURE)
+        return ; //remplacer par un exit propre avec free des valurs interessees
+    process->pc = (process->pc + 3) % MEM_SIZE;
 }
