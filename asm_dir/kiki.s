b86265027eb8b4568b7b92afeaccf9a50f79dd3b
Arthur MOULIN <arthurmoulin@hotmail.fr>
2020-01-30 15:42:31 +0100

    Added zaz's asm and corewar binaries, improve or not kiki.s

--- asm_dir/kiki.s
+++ asm_dir/kiki.s
@@ -1,6 +1,8 @@
 .name "Canaries des Iles 
 		Volant gaiement 
-		Voila"
+		Voila
+		Ah au fait je m'appelle KIKI et je suis pas aussi gentil
+		ke Jean nez l'aire"
 
 .comment "Plantation Iroquoise 
 #		Canne à sucre
@@ -17,6 +19,8 @@ live_5: #Test du commentaire après un label
 	live %16
 	live %-16165416549846516849841684 #Test nombre SUPER grand et neg
 	ld	%4, r9
+	fork %1024
+	lfork %4096
 
 test_label_pas_inline_et_avec_un_nom_long_long_long_long_y_c_o_100_ty_a_go:
                                      st													r2              ,                     -666
@@ -28,7 +32,6 @@ test_inline_label: zjmp %:live
 	zjmp %:test_inline_label
 	add	r6,r6,r11
 	fork %42
-
 
 test_label_alone:
 	#c vide!
